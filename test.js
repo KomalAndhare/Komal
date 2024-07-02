7e5ae7ce53118788cb430a6c39984a96687f8ad1
KomalAndhare <andharekomal4655@gmail.com>
2024-07-02 14:14:46 +0530

    Variable

--- test.js
+++ test.js
@@ -2,5 +2,10 @@ console.log("komal")
 const a="komal"
 let b=1234
 var c="Andhare"
+var d=null
+var e=undefined
 
-console.log("type of"+komal)
+console.log("type of"+a)
+console.log("type of"+c)
+console.log("type of"+e)
+console.log("type of"+d)
